3cb30fa543e768328d675c95d5dfabf509c7c700
ROgbuagu05 <chizzy3003@gmail.com>
2023-09-16 10:30:12 -0700

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,11 +13,11 @@ class FileStorage:
         if cls is None:
             return FileStorage.__objects
         else:
-            filtered_objects = {}
-            for key, obj in FileStorage.__objects.items():
-                if isinstance(obj, cls):
-                    filtered_objects[key] = obj
-                return filtered_objects
+            obj_list = {}
+            for key, val in FileStorage.__objects.items():
+                if cls == val.__class__:
+                    obj_list[key] = val
+                return(obj_list)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
