5c572b006f019ad19d22d7eb135fb4f57ceaa724
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-05 03:55:46 -0700

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,15 +9,4 @@ env.identity = '~/.ssh/school'
 
 def do_clean(number=0):
     """Deletes out-of-date archives"""
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    number = int(number)
