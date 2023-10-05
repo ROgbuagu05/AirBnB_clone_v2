ea3484642d3fbe60b81d85e331d9d3bba6141400
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-05 03:38:36 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,17 +8,16 @@ import os
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
-    try:
-        # Create the versions directory if it doesn't exist
-        local("mkdir -p versions")
+    # Create the versions directory if it doesn't exist
+    local("mkdir -p versions")
 
-        # Create the archive name
-        archive_name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
-        archive_path = "versions/" + archive_name
+    # Create the archive name
+    archive_name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    archive_path = "versions/" + archive_name
 
-        # Compress the web_static folder into the archive
-        result = local("tar -czvf {} web_static".format(archive_path))
-        if result.succeeded:
-             return archive_path
-         else:
-             return None
+    # Compress the web_static folder into the archive
+    result = local("tar -czvf {} web_static".format(archive_path))
+    if result.succeeded:
+        return archive_path
+    else:
+        return None
