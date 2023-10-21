0a4d0d5168b8737da677a3e6b522b864d46a93cd
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-21 15:50:47 -0700

    Task 7 updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
 from models import storage_type
 
+Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.amenity import Amenity
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
